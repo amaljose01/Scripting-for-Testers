965e963b2837c1e1f08f385f004b532f2385fc0c
amaljose01 <amaljom@gmail.com>
2020-01-31 17:57:53 +0530

    Update RestAPIcallsPython.py

--- RestAPIcallsPython.py
+++ RestAPIcallsPython.py
@@ -1,3 +1,6 @@
+##Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+##Type "help", "copyright", "credits" or "license()" for more information.
+
 ##Below are not a single py script- Execute these are individual commands
 ## dummy json files are retrived from URL : https://jsonplaceholder.typicode.com
 
